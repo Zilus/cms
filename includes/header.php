ec0fa99ce29491564f4383bcf0036d65177254e2
Zilus <neozilus@gmail.com>
2016-09-01 15:53:27 -0500

    Uniform removal

--- includes/header.php
+++ includes/header.php
@@ -36,7 +36,6 @@ if($row_ambulance['settings_value']==1) {
 <link href="plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
 <link href="plugins/simple-line-icons/simple-line-icons.min.css" rel="stylesheet" type="text/css"/>
 <link href="plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
-<link href="plugins/uniform/css/uniform.default.css" rel="stylesheet" type="text/css"/> 
 
 <!-- END GLOBAL MANDATORY STYLES -->
 <!-- BEGIN PAGE LEVEL STYLES -->
