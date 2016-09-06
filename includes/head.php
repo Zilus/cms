dd0fd69e525174c781e2267b7e434ac43df191f3
Zilus <neozilus@gmail.com>
2016-09-06 16:11:03 -0500

    Menu bar menu

--- includes/head.php
+++ includes/head.php
@@ -19,6 +19,107 @@
 		<!-- BEGIN TOP NAVIGATION MENU -->
 		<div class="top-menu">
 			<ul class="nav navbar-nav pull-right">
+				<!-- BEGIN INBOX DROPDOWN -->
+				<li class="dropdown dropdown-extended dropdown-inbox" id="header_inbox_bar">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">
+					<i class="fa fa-envelope"></i>
+					<span class="badge badge-default">
+					4 </span>
+					</a>
+					<ul class="dropdown-menu">
+						<li>
+							<p>
+								 Tienes 4 nuevos mensajes
+							</p>
+						</li>
+						<li>
+							<ul class="dropdown-menu-list scroller" style="height: 250px;">
+								<li>
+									<a href="inbox.html?a=view">
+									<span class="photo">
+									<img src="<?php echo $_SESSION['avatar']; ?>" alt=""/>
+									</span>
+									<span class="subject">
+									<span class="from">
+									Lisa Wong </span>
+									<span class="time">
+									Just Now </span>
+									</span>
+									<span class="message">
+									Vivamus sed auctor nibh congue nibh. auctor nibh auctor nibh... </span>
+									</a>
+								</li>
+								<li>
+									<a href="inbox.html?a=view">
+									<span class="photo">
+									<img src="<?php echo $_SESSION['avatar']; ?>" alt=""/>
+									</span>
+									<span class="subject">
+									<span class="from">
+									Richard Doe </span>
+									<span class="time">
+									16 mins </span>
+									</span>
+									<span class="message">
+									Vivamus sed congue nibh auctor nibh congue nibh. auctor nibh auctor nibh... </span>
+									</a>
+								</li>
+								<li>
+									<a href="inbox.html?a=view">
+									<span class="photo">
+									<img src="<?php echo $_SESSION['avatar']; ?>" alt=""/>
+									</span>
+									<span class="subject">
+									<span class="from">
+									Bob Nilson </span>
+									<span class="time">
+									2 hrs </span>
+									</span>
+									<span class="message">
+									Vivamus sed nibh auctor nibh congue nibh. auctor nibh auctor nibh... </span>
+									</a>
+								</li>
+								<li>
+									<a href="inbox.html?a=view">
+									<span class="photo">
+									<img src="<?php echo $_SESSION['avatar']; ?>" alt=""/>
+									</span>
+									<span class="subject">
+									<span class="from">
+									Lisa Wong </span>
+									<span class="time">
+									40 mins </span>
+									</span>
+									<span class="message">
+									Vivamus sed auctor 40% nibh congue nibh... </span>
+									</a>
+								</li>
+								<li>
+									<a href="inbox.html?a=view">
+									<span class="photo">
+									<img src="<?php echo $_SESSION['avatar']; ?>" alt=""/>
+									</span>
+									<span class="subject">
+									<span class="from">
+									Richard Doe </span>
+									<span class="time">
+									46 mins </span>
+									</span>
+									<span class="message">
+									Vivamus sed congue nibh auctor nibh congue nibh. auctor nibh auctor nibh... </span>
+									</a>
+								</li>
+							</ul>
+						</li>
+						<li class="external">
+							<a href="inbox.php">
+							Ver todos los mensajes <i class="m-icon-swapright"></i>
+							</a>
+						</li>
+					</ul>
+				</li>
+				<!-- END INBOX DROPDOWN -->
+				
 				<!-- BEGIN USER LOGIN DROPDOWN -->
 				<li class="dropdown dropdown-user">
 					<a href="#" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">
